6b63e5b1b5de77298832480af9700843fe0e8101
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-07 05:56:33 -0700

    initialized 52 cards added __repr__

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -2,8 +2,6 @@ class Cards:
 	def __init__(self,suit,value):
 		self.suit=suit
 		self.value=value
-
-	@classmethod
 	def __repr__(cls):
 		return("{} of {}".format(self.value,self.suit))
 	@classmethod
@@ -13,3 +11,16 @@ class Deck:
 	suits=["Hearts","Diamonds","Clubs","Spades"]
 	values=["A","2","3","4","5","6","7",
 	"8","9","10","J","Q","K"]
+
+	def __init__(self):
+		self.caard=[]
+		for st in Deck.suits:
+			for st2 in Deck.values:
+				self.caard.append(Cards.bulk_cards(st,st2))
+	def __repr__(self):
+		return ("Deck of {} card's".format(len(self.caard)))
+
+
+deck1= Deck()
+
+print(deck1)
