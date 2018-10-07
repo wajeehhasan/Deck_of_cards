274d94e75c09e49daf8320965b93400276ba3373
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-07 05:36:34 -0700

    class of decks

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -6,4 +6,10 @@ class Cards:
 	@classmethod
 	def __repr__(cls):
 		return("{} of {}".format(self.value,self.suit))
+	@classmethod
+	def bulk_cards(cls,data_suit,data_value):
+		return cls(data_suit,data_value)
 class Deck:
+	suits=["Hearts","Diamonds","Clubs","Spades"]
+	values=["A","2","3","4","5","6","7",
+	"8","9","10","J","Q","K"]
